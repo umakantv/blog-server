ae0ba3dbe410e2d33f336f6833c5bd85b13291cc
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2023-06-30 19:19:01 +0530

    Change Auth URL

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,88 +1,83 @@
-const authService = require('../services/auth.service')
+const authService = require("../services/auth.service");
 
 async function login(req, res, next) {
+  try {
+    const user = req.body;
 
-    try {
-        const user = req.body;
-    
-        let {email, password} = user;
-    
-        let { token } = await authService.login(email, password)
-    
-        return res.status(200).send({
-            status: 'success',
-            data: {
-                token
-            }
-        })
+    let { email, password } = user;
 
-    }  catch(err) {
-        next(err)
-    }
+    let { token } = await authService.login(email, password);
+
+    return res.status(200).send({
+      status: "success",
+      data: {
+        token,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 async function getLoggedInUser(req, res, next) {
+  try {
+    const { user } = req;
 
-    try {
-        const {user} = req;
-    
-        if (user) {
-            return res.status(200).send({
-                status: 'success',
-                data: user
-            })
-        } else {
-            return res.status(400).send({
-                status: 'error',
-                message: 'User not logged in'
-            })
-        }
-    } catch(err) {
-        next(err)
+    if (user) {
+      return res.status(200).send({
+        status: "success",
+        data: user,
+      });
+    } else {
+      return res.status(400).send({
+        status: "error",
+        message: "User not logged in",
+      });
     }
-
+  } catch (err) {
+    next(err);
+  }
 }
 
 async function register(req, res, next) {
-    try {
+  try {
+    let { name, email, password } = req.body;
 
-        let {name, email, password} = req.body;
-    
-        let user = await authService.register({
-            name, email, password
-        })
-    
-        return res.status(200).send({
-            status: 'success',
-            data: user
-        })
+    let user = await authService.register({
+      name,
+      email,
+      password,
+    });
 
-    } catch(err) {
-        next(err)
-    }
+    return res.status(200).send({
+      status: "success",
+      data: user,
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 async function githubSignin(req, res, next) {
+  try {
+    const { code } = req.params;
 
-    try {
-        const {code} = req.query
+    let token = authService.githubSignin(code);
 
-        let token = authService.githubSignin(code);
-
-        return res.status(200).send({
-            status: 'success',
-            data: {
-                token
-            }
-        })
-    } catch(err) {
-        next(err);
-    }
+    return res.status(200).send({
+      status: "success",
+      data: {
+        token,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 module.exports = {
-    register,
-    login,
-    getLoggedInUser,
-    githubSignin,
-}
+  register,
+  login,
+  getLoggedInUser,
+  githubSignin,
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,11 @@
+const express = require("express");
+const authController = require("../controllers/auth.controllers");
 
-const express = require('express')
-const authController = require('../controllers/auth.controllers')
+const authRouter = express.Router();
 
-const authRouter = express.Router()
-
-authRouter.get('/loggedInUser', authController.getLoggedInUser)
-authRouter.post('/login', authController.login)
-authRouter.post('/register', authController.register)
-authRouter.get('/githubSignin', authController.githubSignin)
+authRouter.get("/loggedInUser", authController.getLoggedInUser);
+authRouter.post("/login", authController.login);
+authRouter.post("/register", authController.register);
+authRouter.get("/github-signin/:code", authController.githubSignin);
 
 module.exports = authRouter;
