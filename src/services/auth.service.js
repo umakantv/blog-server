26d6f2348c792917b2e4fd118d43365b30ec1d69
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2023-06-30 18:49:11 +0530

    [Fix]: Login and Register

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,78 +1,89 @@
-const bcrypt = require('bcryptjs');
-const AppError = require('../utils/AppError');
-const GithubService = require('./github');
-const userModel = require('../database/user.model');
+const bcrypt = require("bcryptjs");
+const AppError = require("../utils/AppError");
+const GithubService = require("./github");
+const userModel = require("../database/user.model");
+const { generateToken } = require("../utils/tokens");
 let githubService = new GithubService();
 
-async function login(email, password) {
-
-    let existingUser = await userModel.findOne({
-        email
-    })
-
-    if (existingUser) {
-        let match = bcrypt.compareSync(password, existingUser.password);
-
-        if (match) {
-            // produce a JWT token
-            let token = generateUserToken(existingUser);
-
-            return {user, token}
-
-        } else {
-            throw new AppError('Password is wrong', 400)
-        }
-    } else {
-        throw new AppError('User does not exist with the given email', 400)
-    }
+function generateUserToken(user) {
+  return generateToken(user);
 }
 
-async function register({name, email, password}) {
+async function login(email, password) {
+  let existingUser = await userModel.findOne({
+    email,
+  });
 
-    let existingUser = await userModel.findOne({
-        email
-    })
+  if (existingUser) {
+    let match = bcrypt.compareSync(password, existingUser.password);
 
-    if (existingUser) {
-        throw new AppError('User already exists with the given email', 400)
+    if (match) {
+      // produce a JWT token
+      let user = existingUser.toJSON();
+      delete user.password;
+
+      let token = generateUserToken(user);
+
+      return { user, token };
     } else {
-        password = bcrypt.hashSync(password);
-        let user = await userModel.create({
-            name, email, password
-        })
-
-        user = user.toJSON();
-
-        delete user.password;
-
-        return user;
+      throw new AppError("Password is wrong", 400);
     }
+  } else {
+    throw new AppError("User does not exist with the given email", 400);
+  }
+}
+
+async function register({ name, email, password }) {
+  let existingUser = await userModel.findOne({
+    email,
+  });
+
+  if (existingUser) {
+    throw new AppError("User already exists with the given email", 400);
+  } else {
+    password = bcrypt.hashSync(password);
+    let user = await userModel.create({
+      name,
+      email,
+      password,
+    });
+
+    user = user.toJSON();
+
+    delete user.password;
+
+    return user;
+  }
 }
 
 async function githubSignin(code) {
+  const userDetails = await githubService.getUser(code);
 
-    const userDetails = await githubService.getUser(code);
+  let existingUser = await userModel.findOne({
+    authType: "github",
+    githubUsername: userDetails.login,
+  });
 
-    let existingUser = await userModel.findOne({
-        authType: 'github',
-        githubUsername: userDetails.login
+  if (!existingUser) {
+    existingUser = await userModel.create({
+      authType: "github",
+      name: userDetails.name,
+      githubUsername: userDetails.login,
+      image: userDetails.avatar_url,
+      email: userDetails.email,
     });
+  }
 
-    if (!existingUser) {
-        existingUser = await userModel.create({
-            authType: 'github',
-            name: userDetails.name,
-            githubUsername: userDetails.login,
-            image: userDetails.avatar_url,
-            email: userDetails.email,
-        })
-    }
-    
-    let token = generateUserToken(existingUser);
+  let user = existingUser.toJSON();
+  delete user.password;
 
-    return {token, user: existingUser}
+  let token = generateUserToken(user);
+
+  return { token, user };
 }
 
 module.exports = {
-    login, register, githubSignin,
-}
+  login,
+  register,
+  githubSignin,
+};
